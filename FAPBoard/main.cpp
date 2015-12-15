0426b9dbb8addd3444ab13324f1360fa6e0a48bf
Alexander Hill <me@alxhill.com>
2015-12-15 01:45:41 +0000

    Refactoring, allows maze regeneration

--- FAPBoard/main.cpp
+++ FAPBoard/main.cpp
@@ -25,134 +25,12 @@
 #include "arduino-serial-lib.c"
 #include "arduino-serial-lib.h"
 
-// Here is a small helper for you ! Have a look.
 #include "ResourcePath.hpp"
+#include "maze.cpp"
 
 #define WINDOW_WIDTH 1280
 #define WINDOW_HEIGHT 800
 
-enum Dir {
-    NONE = 0,
-    N = 1 << 1,
-    S = 1 << 2,
-    W = 1 << 3,
-    E = 1 << 4
-};
-
-int cellSize = 40;
-int cellWidth;
-int cellHeight;
-Dir *cells;
-sf::Image *maze;
-
-typedef struct edge {
-    int i; // index
-    Dir d; // direction
-} Edge;
-
-std::vector<Edge*> frontier;
-
-void fillCell(int index, sf::Color color, Dir direction = NONE) {
-    int i = index % cellWidth,
-        j = index / cellWidth;
-
-    sf::Image block;
-    block.create(cellSize, cellSize, color);
-    
-    if (direction & (S | E)) {
-        if (direction == E) {
-            maze->copy(block, (i+1) * (cellSize + cellSize), j * cellSize + (j+1) * cellSize);
-        } else if (direction == S) {
-            maze->copy(block, i*cellSize + (i+1) * cellSize, (j+1)*(cellSize + cellSize));
-        }
-    } else {
-        maze->copy(block, i*cellSize + (i+1) * cellSize, j*cellSize + (j+1)*cellSize);
-    }
-}
-
-// create a simple maze using recursive division method - see wikipedia for details
-sf::Image *createMaze(unsigned int width, unsigned int height) {
-
-    cellWidth = (width-cellSize)/(cellSize + cellSize);
-    cellHeight = (height-cellSize)/(cellSize + cellSize);
-    
-    cells = new Dir[cellWidth * cellHeight]();
-    
-    maze = new sf::Image;
-    frontier.clear();
-    maze->create(width, height, sf::Color::Black);
-    
-    frontier.push_back(new Edge{0, S});
-    frontier.push_back(new Edge{0, E});
-    fillCell(0, sf::Color::White);
-    
-    while (frontier.size() > 0) {
-        Edge *e = frontier.back();
-        frontier.pop_back();
-        
-        int i = e->i;
-        Dir d = e->d;
-        
-        int newI = i + (d == N ? -cellWidth : d == S ? cellWidth : d == W ? -1 : 1);
-        int x = e->i % cellWidth;
-        int y = e->i / cellWidth;
-        int newX, newY, newDir;
-        bool open = cells[newI] == NONE;
-        sf::Color cellColour = open ? sf::Color::White : sf::Color::Black;
-        
-        switch (d) {
-            case N:
-                fillCell(newI, cellColour, S);
-                newX = x;
-                newY = y - 1;
-                newDir = S;
-                break;
-            case S:
-                fillCell(i, cellColour, S);
-                newX = x;
-                newY = y + 1;
-                newDir = N;
-                break;
-            case W:
-                fillCell(newI, cellColour, E);
-                newX = x - 1;
-                newY = y;
-                newDir = E;
-                break;
-            case E:
-                fillCell(i, cellColour, E);
-                newX = x + 1;
-                newY = y;
-                newDir = W;
-                break;
-            default:
-                std::cout << "WTF????\n";
-                break;
-        }
-        
-        if (open) {
-            fillCell(newI, cellColour);
-            cells[i] = (Dir)(d | cells[i]);
-            cells[newI] = (Dir)(d | cells[newI]);
-            
-            if (newY > 0 && cells[newI - cellWidth] == NONE) {
-                frontier.push_back(new Edge{newI, N});
-            }
-            if (newY < cellHeight - 1 && cells[newI + cellWidth] == NONE) {
-                frontier.push_back(new Edge{newI, S});
-            }
-            if (newX > 0 && cells[newI - 1] == NONE) {
-                frontier.push_back(new Edge{newI, W});
-            }
-            if (newX < cellWidth - 1 && cells[newI + 1] == NONE) {
-                frontier.push_back(new Edge{newI, E});
-            }
-            std::random_shuffle(frontier.begin(), frontier.end());
-        }
-    }
-    
-    return maze;
-}
 
 int main(int, char const**)
 {
@@ -160,21 +38,12 @@ int main(int, char const**)
     const char* de = "/dev/cu.usbmodem1421";
     auto sp = serialport_init(de, 9600);
 
-
     // Create the main window
     auto screen = sf::VideoMode::getDesktopMode();
     sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "FAPBoard");
-    sf::Image *image = createMaze(WINDOW_WIDTH, WINDOW_HEIGHT);
-//    image.create(WINDOW_WIDTH, WINDOW_HEIGHT);
-//    for (int x = 0; x < WINDOW_WIDTH; x++) {
-//        for (int y = 0; y < WINDOW_HEIGHT; y++) {
-//            int r = 127*(1-std::cos(2*M_PI*(float)x/WINDOW_WIDTH));
-//            sf::Color randCol(r, r, r);
-//            image.setPixel(x, y, randCol);
-//        }
-//    }
-//    image->loadFromFile(resourcePath() + "maze.bmp");
-    
+    Maze m(WINDOW_WIDTH, WINDOW_HEIGHT);
+    sf::Image *image = m.generate();
+
     // initialise overlay and font
     sf::Image overlay;
     overlay.create(WINDOW_WIDTH, WINDOW_HEIGHT, sf::Color::Transparent);
@@ -184,16 +53,12 @@ int main(int, char const**)
     }
     sf::Text text("?", font, 50);
     text.setColor(sf::Color::Blue);
-    sf::Vector2f targetSize(WINDOW_WIDTH, WINDOW_HEIGHT);
 
     sf::Texture texture;
     texture.loadFromImage(*image);
     //texture.loadFromFile(resourcePath() + "maze.bmp");
 
     sf::Sprite sprite(texture);
-    sprite.setScale(
-                    targetSize.x / sprite.getLocalBounds().width,
-                    targetSize.y / sprite.getLocalBounds().height);
     sf::Texture ovTex;
     ovTex.loadFromImage(overlay);
     sf::Sprite ovSpr(ovTex);
@@ -213,6 +78,11 @@ int main(int, char const**)
             // Escape pressed: exit
             if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape) {
                 window.close();
+            }
+            
+            if (event.type == sf::Event::MouseButtonReleased) {
+                image = m.generate();
+                texture.loadFromImage(*image);
             }
         }
         auto mousePos = sf::Mouse::getPosition();
---
+++ FAPBoard/maze.cpp
@@ -0,0 +1,148 @@
+//
+//  maze.cpp
+//  FAPBoard
+//
+//  Created by Alexander Hill on 15/12/2015.
+//  Copyright © 2015 Alexander Hill. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <SFML/Graphics.hpp>
+
+class Maze {
+public:
+    Maze(int w, int h) {
+        width = w;
+        height = h;
+    }
+private:
+    enum Dir {
+        NONE = 0,
+        N = 1 << 1,
+        S = 1 << 2,
+        W = 1 << 3,
+        E = 1 << 4
+    };
+    
+    typedef struct edge {
+        int i; // index
+        Dir d; // direction
+    } Edge;
+    
+    int cellSize = 40;
+    int cellWidth;
+    int cellHeight;
+    Dir *cells;
+    sf::Image *maze;
+    
+    int width, height;
+    
+    std::vector<Edge*> frontier;
+    
+    void fillCell(int index, sf::Color color, Dir direction = NONE) {
+        int i = index % cellWidth,
+        j = index / cellWidth;
+        
+        sf::Image block;
+        block.create(cellSize, cellSize, color);
+        
+        if (direction & (S | E)) {
+            if (direction == E) {
+                maze->copy(block, (i+1) * (cellSize + cellSize), j * cellSize + (j+1) * cellSize);
+            } else if (direction == S) {
+                maze->copy(block, i*cellSize + (i+1) * cellSize, (j+1)*(cellSize + cellSize));
+            }
+        } else {
+            maze->copy(block, i*cellSize + (i+1) * cellSize, j*cellSize + (j+1)*cellSize);
+        }
+    }
+    
+    void reset() {
+        cellWidth = (width-cellSize)/(cellSize + cellSize);
+        cellHeight = (height-cellSize)/(cellSize + cellSize);
+        
+        cells = new Dir[cellWidth * cellHeight]();
+        
+        maze = new sf::Image;
+        frontier.clear();
+        maze->create(width, height, sf::Color::Black);
+    }
+    
+public:
+    // create a simple maze using recursive division method - see wikipedia for details
+    sf::Image *generate() {
+        reset();
+        frontier.push_back(new Edge{0, S});
+        frontier.push_back(new Edge{0, E});
+        fillCell(0, sf::Color::White);
+        
+        while (frontier.size() > 0) {
+            Edge *e = frontier.back();
+            frontier.pop_back();
+            
+            int i = e->i;
+            Dir d = e->d;
+            
+            int newI = i + (d == N ? -cellWidth : d == S ? cellWidth : d == W ? -1 : 1);
+            int x = e->i % cellWidth;
+            int y = e->i / cellWidth;
+            int newX, newY, newDir;
+            bool open = cells[newI] == NONE;
+            sf::Color cellColour = open ? sf::Color::White : sf::Color::Black;
+            
+            switch (d) {
+                case N:
+                    fillCell(newI, cellColour, S);
+                    newX = x;
+                    newY = y - 1;
+                    newDir = S;
+                    break;
+                case S:
+                    fillCell(i, cellColour, S);
+                    newX = x;
+                    newY = y + 1;
+                    newDir = N;
+                    break;
+                case W:
+                    fillCell(newI, cellColour, E);
+                    newX = x - 1;
+                    newY = y;
+                    newDir = E;
+                    break;
+                case E:
+                    fillCell(i, cellColour, E);
+                    newX = x + 1;
+                    newY = y;
+                    newDir = W;
+                    break;
+                default:
+                    break;
+            }
+            
+            if (open) {
+                fillCell(newI, cellColour);
+                cells[i] = (Dir)(d | cells[i]);
+                cells[newI] = (Dir)(d | cells[newI]);
+                
+                if (newY > 0 && cells[newI - cellWidth] == NONE) {
+                    frontier.push_back(new Edge{newI, N});
+                }
+                if (newY < cellHeight - 1 && cells[newI + cellWidth] == NONE) {
+                    frontier.push_back(new Edge{newI, S});
+                }
+                if (newX > 0 && cells[newI - 1] == NONE) {
+                    frontier.push_back(new Edge{newI, W});
+                }
+                if (newX < cellWidth - 1 && cells[newI + 1] == NONE) {
+                    frontier.push_back(new Edge{newI, E});
+                }
+                std::random_shuffle(frontier.begin(), frontier.end());
+            }
+        }
+        
+        return maze;
+    }
+    
+};
